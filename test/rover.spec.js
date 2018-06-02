2c7db080f2c8524c965bf43efbd7ce54f1ea45f7
Lin Zehang <zhlin@thoughtworks.com>
2018-06-02 20:20:44 +0800

    feat: add test for mark rover as dead

--- test/rover.spec.js
+++ test/rover.spec.js
@@ -13,3 +13,19 @@ each([
 ]).then('Rover should move to correct position', (input, expected) => {
   assert.strictEqual(input, expected);
 });
+
+
+// test markAsDead
+const deadRover = new Rover(1, 2, 'E');
+
+deadRover.markAsDead();
+
+deadRover.move('MMM');
+each([
+  [deadRover.x, 1],
+  [deadRover.y, 2],
+  [deadRover.direction, 'E'],
+  [deadRover.isDead, true],
+]).then('Rover is dead should not move', (input, expected) => {
+  assert.strictEqual(input, expected);
+});
